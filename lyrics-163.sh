2ee35354ca685e21eaaa527004d602354ea73bad
Tianyi Shi <ShiTianyi2001@outlook.com>
2021-03-30 01:39:13 +0800

    Update and rename netease-lyrics.sh to lyrics-163.sh

--- lyrics-163.sh
+++ lyrics-163.sh
@@ -1,6 +1,6 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-USAGE="USAGE: ${0##*/} music_id/search_keyword [output_file] "
+USAGE="USAGE: ${0##*/} MUSIC_ID|SEARCH_KEYWORD [OUTPUT_FILENAME] "
 
 [[ $# -lt 1 || $1 == '-h' || $1 == '--help' ]] && echo $USAGE && exit 1
 
@@ -11,10 +11,10 @@ fetch_lyric() {
 re='^[0-9]+$'
 if [[ $1 =~ $re ]]; then
     ID=$1
-    OUT=${2:-_lyric.lrc}
+    OUT="${2:-_lyric.lrc}"
     fetch_lyric $ID >${OUT}
 else
-    QUERY=$1
+    QUERY="${@:1}"
     RESULT=$(curl -G --data-urlencode "keywords=$QUERY" https://musicapi.leanapp.cn/search | jq '.result.songs[].id')
     for id in $RESULT; do
         LYRICS=$(fetch_lyric $id)
@@ -22,14 +22,14 @@ else
         echo "Save this one? (y/n)"
         read -rsn1 </dev/tty
         if [[ $REPLY == 'y' ]]; then
-            echo "Save to? ($1.lrc)"
+            echo "Save to? ($QUERY.lrc)"
             read -r </dev/tty
             if [[ -z $REPLY ]]; then
-                OUT=$1.lrc
+                OUT="$QUERY.lrc"
             else
                 OUT=$REPLY
             fi
-            echo "$LYRICS" >$OUT
+            echo "$LYRICS" >"$OUT"
             exit 0
         fi
     done
